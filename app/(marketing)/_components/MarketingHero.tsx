e9d21eb4d56dfad30aa9c5332ded02245b3288d7
Siddhartha Shrestha <sdhrtivv@gmail.com>
2024-05-23 09:25:17 +0545

    changed marketing hero design

--- app/(marketing)/_components/MarketingHero.tsx
+++ app/(marketing)/_components/MarketingHero.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 function MarketingHero() {
   return (
-    <div className="flex flex-col md:flex-row items-center w-fit m-4 md:mx-20 lg:mx-40 bg-neutral-200 rounded-xl">
-      <h1 className="text-xl md:text-2xl lg:text-3xl md:leading-10 font-bold tracking-tighter p-8 h-min">
-        Discover Better Health: Products and Ideas for Well-Being.
+    <div className="flex flex-col md:flex-row items-center w-fit m-4 md:mx-20 lg:mx-40 bg-red-50/50 rounded-xl">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl md:leading-10 font-bold tracking-tighter p-8 h-min break-normal text-[#fc3c05]">
+        Discover Better Health
+        <br /> Products and Ideas for Well-Being.
       </h1>
       <Image
         className="rounded-lg"
-        src={"/images/idea.jpg"}
+        src={"/images/mask.jpg"}
         alt="idea"
         width={450}
         height={450}
