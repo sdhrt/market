90877078610e3cc10ee47847b588f8682b672ab3
Siddhartha Shrestha <sdhrtivv@gmail.com>
2024-05-23 09:44:51 +0545

    changed design

--- app/(marketing)/_components/MarketingHero.tsx
+++ app/(marketing)/_components/MarketingHero.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 function MarketingHero() {
   return (
-    <div className="flex flex-col md:flex-row items-center w-fit m-4 md:mx-20 lg:mx-40 bg-red-50/50 rounded-xl">
-      <h1 className="text-2xl md:text-3xl lg:text-4xl md:leading-10 font-bold tracking-tighter p-8 h-min break-normal text-[#fc3c05]">
+    <div className="flex flex-col md:flex-row justify-between items-center m-4 md:mx-20 lg:mx-40 bg-red-50/50 rounded-xl">
+      <h1
+        className="md:text-3xl lg:text-4xl md:leading-10
+                flex justify-start
+                font-bold tracking-tighter p-8 break-normal text-[#fc3c05]"
+      >
         Discover Better Health
         <br /> Products and Ideas for Well-Being.
       </h1>
